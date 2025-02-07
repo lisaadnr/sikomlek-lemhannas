6eb93b108d205e6d5bc5a629604abe7259ae1ce7
Muhammad Praditya Maulana <pradityamaulana@ubuntu23.04pradit>
2025-02-07 22:39:21 +0700

    Update Title

--- resources/views/pages/tentang.blade.php
+++ resources/views/pages/tentang.blade.php
@@ -17,7 +17,6 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <!-- Title -->
     <title>Stok | SiKomLek</title>
-
 </head>
 
 <body class="hold-transition sidebar-mini sidebar-collapse">
