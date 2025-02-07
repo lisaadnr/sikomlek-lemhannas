2c9fbac3a9f34b2fce802b23f015e96a80fca3f6
Muhammad Praditya Maulana <pradityamaulana@ubuntu23.04pradit>
2025-02-08 00:19:45 +0700

    Update Page Error

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -27,4 +27,12 @@ class Handler extends ExceptionHandler
             //
         });
     }
+
+    public function render($request, Throwable $exception)
+    {
+        if ($this->isHttpException($exception)) {
+            return response()->view("errors.{$exception->getStatusCode()}", [], $exception->getStatusCode());
+        }
+        return parent::render($request, $exception);
+    }
 }
--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -3,9 +3,23 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>404 Error</title>
+    @vite(['resources/css/style.css'])
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
-    
+    <div class="custom-bg">
+        <div class="d-flex align-items-center justify-content-center min-vh-100 px-2">
+            <div class="text-center text-white animate-fade-in">
+                <h1 class="display-1 fw-bold error-title mb-4">404</h1>
+                <p class="fs-2 fw-medium mb-3">Ups! Halaman tidak ditemukan</p>
+                <p class="mb-5">Halaman yang anda cari tidak ada atau sudah dipindahkan</p>
+                <a href="{{ route('pages.dashboard') }}" class="btn  btn-outline-light custom-btn fw-semibold rounded-pill px-4 py-2">
+                    Ke Dashboard
+                </a>
+            </div>
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
