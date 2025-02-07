8127606dca48b53ee4ab57c5a4bbe4d070ce069b
Muhammad Praditya Maulana <pradityamaulana@ubuntu23.04pradit>
2025-02-07 22:44:56 +0700

    Update Title

--- resources/views/pages/inventory.blade.php
+++ resources/views/pages/inventory.blade.php
@@ -15,7 +15,7 @@
     @vite(['resources/css/adminlte.css'])
     <!-- Bootstrap 4 -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-
+    <!-- Title -->
     <title>Inventory | SisInfoKomLek</title>
 
 </head>
